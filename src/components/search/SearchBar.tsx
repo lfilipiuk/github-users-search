5077e65b719724f0f1edc3c4d05f2fdfa1532b11
Łukasz Filipiuk <luke.filipiuk@gmail.com>
2023-04-28 20:40:28 +0200

    Remove default clear button from search input

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -35,7 +35,7 @@ const SearchBar = () => {
         <input
           type="search"
           id="default-search"
-          className="block w-full h-12 p-2 pl-10 text-md text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+          className="block w-full h-12 p-2 pl-10 text-md text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 searchInput"
           placeholder="Search users..."
           value={searchQuery}
           onChange={handleSearchInput}
